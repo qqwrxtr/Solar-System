a05ef9b48af3a05006e46430e6ba7ed81a9b18af
qqwrxtr <teodorghirba1@gmail.com>
2024-08-02 12:48:07 +0300

    no,too bad

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import SolarSystem from './components/SolarSystem/Solarsystem';
 import Stars from './components/Stars/Stars';
 
 function App() {
   return (
     <div className="App">
-      <div className='stars'>
-        <Stars/>
-      </div>
-      <div className='solar_system_showcase'>
-        <SolarSystem/>
-      </div>
+      <Stars/>
     </div>
   );
 }
